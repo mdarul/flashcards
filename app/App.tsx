18117c978be2f12df72d5afc861ab288e06d4033
mdarul <mdarul618@gmail.com>
2022-08-10 17:55:48 +0200

    eslint and husky proper setup

--- app/App.tsx
+++ app/App.tsx
@@ -3,9 +3,9 @@ import store from './src/services/redux/store';
 import Routing from './src/Routing';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <Routing />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<Routing />
+		</Provider>
+	);
 }
